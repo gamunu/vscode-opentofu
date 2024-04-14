a3c6de1ee00487fd273483e977ab6a4b41f692c4
Gamunu Balagalla <gamunu@fastcode.io>
2024-04-14 13:18:57 +0530

    fix: change opentofu install action

--- build/downloader.ts
+++ build/downloader.ts
@@ -6,7 +6,16 @@
 import got from 'got';
 import * as fs from 'fs';
 import * as path from 'path';
-import { fetchVersion } from './fetch';
+import Downloader from 'nodejs-file-downloader';
+
+export interface Release {
+  repository: string;
+  package: string;
+  destination: string;
+  fileName: string;
+  version: string;
+  extract: boolean;
+}
 
 function getPlatform(platform: string) {
   if (platform === 'win32') {
@@ -120,6 +129,52 @@ async function downloadSyntax(info: ExtensionInfo) {
   fs.writeFileSync(fpath, content);
   if (process.env.downloader_log === 'true') {
     console.log(`Download completed: ${fpath}`);
+  }
+}
+
+export async function fetchVersion(release: Release): Promise<void> {
+  validateRelease(release);
+  await downloadFile(release);
+}
+
+async function downloadFile(release: Release) {
+  const url = `https://github.com/${release.repository}/releases/download/v${release.version}/${release.package}`;
+  const downloader = new Downloader({
+    url: url, //If the file name already exists, a new file with the name 200MB1.zip is created.
+    directory: release.destination, //This folder will be created, if it doesn't exist.
+    fileName: release.fileName,
+  });
+
+  try {
+    const dStatus = await downloader.download(); //Downloader.download() resolves with some useful properties.
+
+    if (os !== 'windows' && dStatus.filePath) {
+      fs.chmodSync(dStatus.filePath, 0o744);
+    }
+
+    if (process.env.downloader_log === 'true') {
+      console.log(`Download completed`);
+    }
+  } catch (error) {
+    throw new Error(`Release download failed version: ${release.version}, fileName: ${release.fileName}`);
+  }
+}
+
+function validateRelease(release: Release) {
+  if (!release.repository) {
+    throw new Error('Missing release repository');
+  }
+
+  if (!release.package) {
+    throw new Error('Missing release package name');
+  }
+
+  if (!release.destination) {
+    throw new Error('Missing release destination');
+  }
+
+  if (!release.version) {
+    throw new Error('Missing release version');
   }
 }
 
